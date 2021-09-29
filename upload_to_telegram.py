60af1262cd636c64d931d115a221f26c4aa75a19
George-Salt <solanikg3@gmail.com>
2021-09-29 10:53:19 +0300

    Сократил функцию random_path

--- upload_to_telegram.py
+++ upload_to_telegram.py
@@ -8,20 +8,12 @@ import random
 
 def bot_settings(bot_token, chat_id):
     bot = telegram.Bot(token=bot_token)
-    bot.send_document(chat_id=chat_id, document=open(random_path(), 'rb'))
+    bot.send_document(chat_id=chat_id, document=open(random_path(["nasa", "epic", "spacex"]), 'rb'))
     return "Отправлено"
 
 
-def random_path():
-    random_directory_num = random.randint(1, 3)
-    if random_directory_num == 1:
-        random_directory = "nasa"
-        random_picture = random.sample(listdir("nasa"), 1)
-    if random_directory_num == 2:
-        random_directory = "epic"
-        random_picture = random.sample(listdir("epic"), 1)
-    if random_directory_num == 3:
-        random_directory = "spacex"
-        random_picture = random.sample(listdir("spacex"), 1)
-    resalt_path = f"{random_directory}/{random_picture[0]}"
+def random_path(dirs):
+    random_directory = random.choice(dirs)
+    random_picture = random.choice(listdir(random_directory))
+    resalt_path = f"{random_directory}/{random_picture}"
     return resalt_path

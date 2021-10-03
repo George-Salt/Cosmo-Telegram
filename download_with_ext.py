a5968ee2e7e1c30dc5bc72fc5519b79206a76e32
George-Salt <solanikg3@gmail.com>
2021-10-03 17:20:41 +0300

    Укоротил строки

---
+++ download_with_ext.py
@@ -0,0 +1,19 @@
+from urllib.parse import urlparse
+import requests
+import os
+
+
+def download_image(directory, name, image_url):
+    filepath = f"{directory}/{name}{get_extension(image_url)}"
+
+    response = requests.get(image_url)
+    response.raise_for_status()
+
+    with open(filepath, "wb") as file:
+        file.write(response.content)
+
+
+def get_extension(image_url):
+    parsed_url = urlparse(image_url)
+    extension = os.path.splitext(parsed_url.path)[1]
+    return extension
--- fetch_nasa.py
+++ fetch_nasa.py
@@ -3,7 +3,7 @@ import download_with_ext
 
 
 def download_nasa_epic(token, epic_dir):
-    epic_url = "https://api.nasa.gov/EPIC/api/natural/images?api_key={}".format(token)
+    epic_url = f"https://api.nasa.gov/EPIC/api/natural/images?api_key={token}"
 
     response = requests.get(epic_url)
     images_description = response.json()
@@ -11,14 +11,14 @@ def download_nasa_epic(token, epic_dir):
     for image_num, image in enumerate(images_description[:10]):
         epic_image_name = image["image"]
         epic_image_date = image["date"][:10].replace("-", "/")
-        image_url = "https://api.nasa.gov/EPIC/archive/natural/{linkdate}/png/{name}.png?api_key={token}".format(linkdate = epic_image_date, name=epic_image_name, token=token)
+        image_url = f"https://api.nasa.gov/EPIC/archive/natural/{epic_image_date}/png/{epic_image_name}.png?api_key={token}"
         download_with_ext.download_image(epic_dir, f"{epic_dir}{image_num}", image_url)
 
     return "Загружено - EPIC"
 
 
 def download_nasa_apod(token, apod_dir):
-    apod_url = "https://api.nasa.gov/planetary/apod?api_key={}".format(token)
+    apod_url = f"https://api.nasa.gov/planetary/apod?api_key={token}"
     params = {
         "count": 30
     }

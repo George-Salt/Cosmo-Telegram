511ecb0e7fdd0b248d9d94c4ab2f17f520f27cac
George-Salt <solanikg3@gmail.com>
2021-10-04 10:33:03 +0300

    Добавил payload в GET-параметрах

--- download_with_ext.py
+++ download_with_ext.py
@@ -3,10 +3,10 @@ import requests
 import os
 
 
-def download_image(directory, name, image_url):
+def download_image(directory, name, image_url, payload={}):
     filepath = f"{directory}/{name}{get_extension(image_url)}"
 
-    response = requests.get(image_url)
+    response = requests.get(image_url, params=payload)
     response.raise_for_status()
 
     with open(filepath, "wb") as file:
--- fetch_nasa.py
+++ fetch_nasa.py
@@ -3,26 +3,25 @@ import download_with_ext
 
 
 def download_nasa_epic(token, epic_dir):
-    epic_url = f"https://api.nasa.gov/EPIC/api/natural/images?api_key={token}"
+    epic_url = "https://api.nasa.gov/EPIC/api/natural/images"
+    payload = {"api_key": token}
 
-    response = requests.get(epic_url)
+    response = requests.get(epic_url, params=payload)
     images_description = response.json()
 
     for image_num, image in enumerate(images_description[:10]):
         epic_image_name = image["image"]
         epic_image_date = image["date"][:10].replace("-", "/")
-        image_url = f"https://api.nasa.gov/EPIC/archive/natural/{epic_image_date}/png/{epic_image_name}.png?api_key={token}"
-        download_with_ext.download_image(epic_dir, f"{epic_dir}{image_num}", image_url)
+        image_url = f"https://api.nasa.gov/EPIC/archive/natural/{epic_image_date}/png/{epic_image_name}.png"
+        download_with_ext.download_image(epic_dir, f"{epic_dir}{image_num}", image_url, payload)
 
     return "Загружено - EPIC"
 
 
 def download_nasa_apod(token, apod_dir):
-    apod_url = f"https://api.nasa.gov/planetary/apod?api_key={token}"
-    params = {
-        "count": 30
-    }
-    response = requests.get(apod_url, params=params)
+    apod_url = f"https://api.nasa.gov/planetary/apod"
+    payload = {"count": 30, "api_key": token}
+    response = requests.get(apod_url, params=payload)
     apod_images = response.json()
 
     for image_num, image_url in enumerate(apod_images):

d21081a1681aa0a8b62bb00f37977e005132b27e
George-Salt <solanikg3@gmail.com>
2021-10-06 10:32:31 +0300

    Переименовал функции

--- upload_to_telegram.py
+++ upload_to_telegram.py
@@ -3,14 +3,14 @@ import telegram
 import random
 
 
-def bot_settings(bot_token, chat_id, dirs):
+def send_picture(bot_token, chat_id, dirs):
     bot = telegram.Bot(token=bot_token)
-    with open(random_path(dirs), "rb") as file:
+    with open(fetch_random_path(dirs), "rb") as file:
         bot.send_document(chat_id=chat_id, document=file)
     return "Отправлено"
 
 
-def random_path(dirs):
+def fetch_random_path(dirs):
     random_directory = random.choice(dirs)
     random_picture = random.choice(listdir(random_directory))
     resalt_path = f"{random_directory}/{random_picture}"

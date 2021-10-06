04d00d992ff98212ec8666e02d9f33a6cdfc2a61
George-Salt <solanikg3@gmail.com>
2021-10-06 10:43:14 +0300

    Переименовал переменную с картинками последнего запуска

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -5,7 +5,7 @@ import download_with_ext
 def fetch_last_launch(spacex_dir):
     spaceX_url = "https://api.spacexdata.com/v4/launches/latest"
     response = requests.get(spaceX_url)
-    latest_launches_images = response.json()["links"]["flickr"]["original"]
+    latest_launch_images = response.json()["links"]["flickr"]["original"]
 
-    for image_num, image_url in enumerate(latest_launches_images):
+    for image_num, image_url in enumerate(latest_launch_images):
         download_with_ext.download_image(spacex_dir, f"{spacex_dir}{image_num}", image_url)
